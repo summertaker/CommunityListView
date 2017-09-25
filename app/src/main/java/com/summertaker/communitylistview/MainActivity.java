e587fbb9fa81f8d33fbf087ebe0397644ef40030
Summertaker <summertaker@gmail.com>
2017-09-25 15:12:06 +0900

    Change FAB Color.

--- app/src/main/java/com/summertaker/communitylistview/MainActivity.java
+++ app/src/main/java/com/summertaker/communitylistview/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
